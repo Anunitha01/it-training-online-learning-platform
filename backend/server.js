012f6804471a8384d5de43f6cdc90f88d6d675f9
Anunitha <anunitha01@gmail.com>
2026-01-09 03:01:17 +0800

    6

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const faqRoute = require("./routes/faq");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 
 // Test route
@@ -18,9 +18,7 @@ app.get("/", (req, res) => {
 app.use("/courses", coursesRoute);
 app.use("/faq", faqRoute);
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
-
-
